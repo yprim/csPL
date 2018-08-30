9911db4bf80da646f25688daa884de6c3a9d32a9
yprim <priscipo@gmail.com>
2018-08-30 11:57:56 -0600

    add files

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.ReadLine();
+            Console.WriteLine("Digite su nombre"); 
+            string nombre = Console.ReadLine(); 
+            Console.WriteLine("Hola " + nombre);
+           
+
+        }
+    }
+}
